9c405eb30de14e0aa5ad079af41b93516109c545
chekaaaaa <muellercornelius1@gmail.com>
2019-05-16 10:22:41 +0200

    changed links

--- js/routes.js
+++ js/routes.js
@@ -1,23 +1,23 @@
 const routes = [
     {
         path: 'home',
-        file: '/Island-Blog/views/home.html',
+        file: 'views/home.html',
         initial: true,
         shouldPrefetch: true,
     },
     {
         path: 'touren',
-        file: '/Island-Blog/views/touren.html',
+        file: 'views/touren.html',
         shouldPrefetch: true,
     },
     {
         path: 'gallery',
-        file: '/Island-Blog/views/gallery.html',
+        file: 'views/gallery.html',
         shouldPrefetch: true,
     },
     {
         path: 'map',
-        file: '/Island-Blog/views/map.html',
+        file: 'views/map.html',
         shouldPrefetch: true,
     },
 ];
